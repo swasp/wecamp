8b6e23ab2d279e41b793c827abfea76192e771d3
Henri de Jong <henri.de.jong@infi.nl>
2017-08-24 11:59:42 +0200

    have flight info in app.js

--- web/src/FlightForm.js
+++ web/src/FlightForm.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import Flight from './Flight';
-import ReactDOM from 'react-dom';
 import './App.css';
 
 class FlightForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: '', flight: null};
+        this.state = {flightNumber: '', date: ''};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleFlightNumberChange = this.handleFlightNumberChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value.toUpperCase()});
+    handleFlightNumberChange(event) {
+        this.setState({flightNumber: event.target.value.toUpperCase()});
+    }
+
+    handleDateChange(event) {
+        this.setState({date: event.target.value});
     }
 
     handleSubmit(event) {
-        alert('A flight number was submitted: ' + this.state.value);
-        // var flight = Flight;
+        alert('You entered flight number: ' + this.state.flightNumber + ' on ' + this.state.date);
+        //this.setState({flightNumber: this.state.value});
+        this.props.callbackFromParent(this.state.flightNumber, this.state.date);
+        //var flight = Flight.props({flightNo: this.state.value});
         // flight.setFlight(this.state.value);
-        // this.setState({flight: flight});
+        //this.setState({flight: flight});
         //  ReactDOM.render(
         //      this.state.flight,
         //      document.getElementById('flight')
@@ -33,9 +38,17 @@ class FlightForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <h3>Please enter your flight number</h3>
                 <label>
-                    Flight Number:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <div>Flight Number:</div>
+                    <input type="text" value={this.state.value} onChange={this.handleFlightNumberChange} />
                 </label>
+                <br />
+                <br />
+                <label>
+                    <div>Date:</div>
+                    <input type="date" value={this.state.date} onChange={this.handleDateChange} />
+                </label>
+                <br/>
+                <br/>
                 <input type="submit" value="Submit" />
                 <div id="flight"></div>
             </form>
